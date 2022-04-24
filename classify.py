566c11eaf3a88ef3fb357035c42ffa690f4e750c
fengqing <fengqingthu@gmail.com>
2022-04-24 01:54:27 -0400

    Added softmax

--- classify.py
+++ classify.py
@@ -35,7 +35,7 @@ def classify(model_id, data_dir, checkpoint, npoints, dims=6, nclasses=40):
         # skip non-obj files
         if not file.endswith(".txt"):
             continue
-        order.append(file.split('_')[0])
+        order.append(file.split('_')[1].split('.')[0])
         xyz_points = np.loadtxt(data_dir+'/'+file, delimiter=',')
 
         if npoints > 0:
@@ -47,57 +47,30 @@ def classify(model_id, data_dir, checkpoint, npoints, dims=6, nclasses=40):
         # normalize
         xyz_points[:, :3] = pc_normalize(xyz_points[:, :3])
 
-        print(xyz_points.shape)
+        # print(xyz_points.shape)
         data.append(xyz_points)
     
-    data = torch.Tensor(np.ndarray(data))
+    data = torch.Tensor(np.stack(data))
+    print(data.shape)
     test_loader = DataLoader(dataset=TensorDataset(data),
                              batch_size=1, shuffle=False,
                              num_workers=1)
+    
     print('Constructing dataloader completed')
 
     i = 0
-    for data in tqdm(test_loader):
-        xyz, points = data[:, :, :3], torch.zeros_like(data[:, :, 3:]) # do not use normals
+    m = nn.softmax(dim=1)
+    for batch in tqdm(test_loader):
+        # print(data.shape)
+        data = batch[0]
+        xyz = data[:, :, :3]
+        points = torch.zeros_like(xyz) # do not use normals
         with torch.no_grad():
             pred = model(xyz.to(device), points.to(device))
-            pred = torch.max(pred, dim=-1)[1]
-        print('Sample {} pred: {}'.format(order[i], pred))
+            pred = m(pred)
+            # print(cls2name[torch.max(pred, dim=-1)[1]])
+        print('Sample {} plantness: {}'.format(order[i], pred[0,26]))
         i += 1
-
-    # for file in os.listdir(data_dir):
-    #         # skip non-obj files
-    #         if not file.endswith(".txt"):
-    #             continue
-    #         xyz_points = np.loadtxt(file, delimiter=',')
-    #         if npoints > 0:
-    #             inds = np.random.randint(0, len(xyz_points), size=(npoints, ))
-    #         else:
-    #             inds = np.arange(len(xyz_points))
-    #             np.random.shuffle(inds)
-    #         xyz_points = xyz_points[inds, :]
-    #         # normalize
-    #         xyz_points[:, :3] = pc_normalize(xyz_points[:, :3])
-    #         xyz, points = xyz_points[:, :, :3], xyz_points[:, :, :3]
-    #         with torch.no_grad():
-    #             pred = model(xyz.to(device), points.to(device))
-    #             pred = torch.max(pred, dim=-1)[1]
-    #         print('{} pred: {}'.format(file, pred))
-
-
-    
-
-    # total_correct, total_seen = 0, 0
-    # for data, labels in tqdm(test_loader):
-    #     labels = labels.to(device)
-    #     xyz, points = data[:, :, :3], data[:, :, 3:]
-    #     with torch.no_grad():
-    #         pred = model(xyz.to(device), points.to(device))
-    #         pred = torch.max(pred, dim=-1)[1]
-    #         total_correct += torch.sum(pred == labels)
-    #         total_seen += xyz.shape[0]
-    # print("Evaluating completed!")
-    # print('Corr: {}, Seen: {}, Acc: {:.4f}'.format(total_correct, total_seen, total_correct / float(total_seen)))
 
 if __name__ == '__main__':
     fire.Fire()
